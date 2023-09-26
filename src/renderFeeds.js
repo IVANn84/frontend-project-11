0a71732c507377cca127989f1841e3b997311c87
Ivan Khristenko <ivan.tihii1984@gmail.com>
2023-09-26 15:54:13 +0300

    fix

--- src/renderFeeds.js
+++ src/renderFeeds.js
@@ -1,30 +1,39 @@
-export default (state, elements, i18next) => {
-  elements.containerFeeds.innerHTML = '';
-  const divContainer = document.createElement('div');
-  const divTitle = document.createElement('div');
-  const ul = document.createElement('ul');
-  const h2 = document.createElement('h2');
-
-  h2.classList.add('card-title', 'h4');
-  h2.textContent = i18next.t('renderFeeds.header');
-  divContainer.classList.add('card', 'border-0');
-  divTitle.classList.add('card-body');
-  divTitle.append(h2);
-  ul.classList.add('list-group', 'border-0', 'rounded-0');
-  divContainer.append(divTitle, ul);
-  elements.containerFeeds.append(divContainer);
-
-  state.feeds.forEach((feed) => {
+export default (feedsList, elements, i18next) => {
+  const div = document.createElement('div');
+  div.classList.add('list-group', 'border-0', 'rounded-0');
+  feedsList.forEach((feed) => {
     const li = document.createElement('li');
-    const p = document.createElement('p');
-    const h3 = document.createElement('h3');
     li.classList.add('list-group-item', 'border-0', 'border-end-0');
-    h3.classList.add('h6', 'm-0');
-    p.classList.add('m-0', 'small', 'text-black-50');
-    p.textContent = feed.description;
-    li.prepend(p);
-    li.prepend(h3);
-    h3.textContent = feed.title;
-    ul.prepend(li);
+
+    const header = document.createElement('h3');
+    header.classList.add('h6', 'm-0');
+    header.textContent = feed.title;
+
+    const description = document.createElement('p');
+    description.classList.add('m-0', 'small', 'text-black-50');
+    description.textContent = feed.description;
+
+    li.prepend(description);
+    li.prepend(header);
+
+    div.prepend(li);
   });
+
+  const title = document.createElement('h2');
+
+  title.classList.add('card-title', 'h4');
+  title.textContent = i18next.t('renderFeeds.header');
+
+  const cardBody = document.createElement('div');
+  cardBody.classList.add('card-body');
+  cardBody.prepend(title);
+
+  const card = document.createElement('div');
+  card.classList.add('card', 'border-0');
+
+  card.prepend(cardBody);
+  card.append(div);
+
+  const { feeds } = elements;
+  feeds.replaceChildren(card);
 };
