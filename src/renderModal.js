4ddabe8299959fe0919d0f7e26341f45c8e3d964
Ivan Khristenko <ivan.tihii1984@gmail.com>
2023-09-26 09:47:43 +0300

    fix

--- src/renderModal.js
+++ src/renderModal.js
@@ -1,15 +1,18 @@
-export default (state, postId) => {
+export default (state, postId, elements) => {
   //  const postId = state.ui.id
+  const {
+    modal, modalTitle, modalBody, modalLink,
+  } = elements;
   const currentPost = state.posts.find((post) => post.id === postId);
   const {
     id, title, description, link,
   } = currentPost;
 
-  const modal = document.querySelector('.modal');
+  // const modal = document.querySelector('.modal');
 
-  const modalTitle = document.querySelector('.modal-title');
-  const modalBody = document.querySelector('.modal-body');
-  const modalLink = document.querySelector('.modal-link');
+  // const modalTitle = document.querySelector('.modal-title');
+  // const modalBody = document.querySelector('.modal-body');
+  // const modalLink = document.querySelector('.modal-link');
 
   modal.setAttribute('data-id', id);
   modalTitle.textContent = title;
